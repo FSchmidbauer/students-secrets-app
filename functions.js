6c772ef3d954630820b0a678b8113bbde6c5d808
Florian Schmidbauer <f.schmidbauer@me.com>
2021-04-27 14:08:45 +0200

    new student infos added

---
+++ functions.js
@@ -0,0 +1,5 @@
+const pageviews = document.querySelector('.pageviews');
+let counter = 0;
+pageviews.innerText = counter;
+
+window.addEventListener("load",() => pageviews.innerText=pageviews.innerText+1);
