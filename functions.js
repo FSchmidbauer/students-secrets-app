24fa36e179d7a133124d694d7f71bf628172f9b1
Florian Schmidbauer <f.schmidbauer@me.com>
2021-05-01 01:47:37 +0200

    final version

--- functions.js
+++
@@ -1,6 +0,0 @@
-const pageviews = document.querySelector('.pageviews__content');
-let counter = 0;
-pageviews.innerText = counter;
-
-window.addEventListener("load",() => pageviews.innerText=pageviews.innerText+1);
-
